6d6678bad0cdea0b79202247ce8bb46ef4c03b39
Philipp Stappert <philipp.stappert@student.kit.edu>
2023-11-15 14:53:00 +0100

    tests and create landing page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import RoomOverviewView from '../views/RoomOverviewView.vue';
+import LandingPageView from '@/views/LandingPageView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: RoomOverviewView
+    },
+    {
+      path: '/welcome',
+      name: 'landing-page',
+      component: LandingPageView
     }
   ]
 })
---
+++ src/views/LandingPageView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+//TODO implement landing page
+</script>
+<template>
+  <p>Hello World!</p>
+</template>
