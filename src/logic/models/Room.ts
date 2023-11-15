1c32861107ed40dab2fbbfa11a80af5a6d02772e
ubpch <ubpch@student.kit.edu>
2023-11-15 16:59:09 +0100

    Added Datenmodell für Räume

--- src/logic/models/Room.ts
+++ src/logic/models/Room.ts
@@ -1,3 +1,33 @@
 class Room{
-  //TODO
+
+  constructor(roomName: string, roomId: string, roomDescription: string, roomPictureUrl: string) {
+    this.roomName = roomName;
+    this.roomId = roomId;
+    this.roomDescription = roomDescription;
+    this.roomPictureUrl = roomPictureUrl;
+  }
+
+  private roomName: string;
+  private roomId: string;
+  private roomDescription: string;
+  private roomPictureUrl: string;
+
+  public getRoomName(): string {
+    return this.roomName;
+  }
+
+  public getRoomId(): string {
+    return this.roomId;
+  }
+
+  public getRoomDescription(): string {
+    return this.roomDescription;
+  }
+
+  public getRoomPictrueUrl(): string {
+    return this.roomPictureUrl;
+  }
+
 }
+
+export default Room
