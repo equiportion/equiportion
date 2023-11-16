5481a489eb8750c7c7791df1ee2b4404ef69fe1a
Philipp Stappert <philipp.stappert@student.kit.edu>
2023-11-16 15:22:55 +0100

    add some base components needed for #32

---
+++ src/components/steps/StepsBar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <div
+      class="relative after:absolute after:inset-x-0 after:top-1/2 after:block after:h-0.5 after:-translate-y-1/2 after:rounded-lg after:bg-gray-100"
+    >
+      <ol class="relative z-10 flex justify-between text-sm font-medium text-gray-500">
+        <slot />
+      </ol>
+    </div>
+  </div>
+</template>
---
+++ src/components/steps/StepsBarStep.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import {computed} from 'vue';
+
+const props = defineProps({
+  active: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  completed: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+
+const spanClasses = computed(() => {
+  if (props.active) {
+    return 'h-6 w-6 rounded-full bg-blue-600 text-center text-[10px]/6 font-bold text-white';
+  } else if (props.completed) {
+    return 'h-6 w-6 rounded-full bg-green-500 text-center text-[10px]/6 font-bold text-white';
+  }
+  return 'h-6 w-6 rounded-full bg-gray-100 text-center text-[10px]/6 font-bold';
+});
+
+const textClasses = computed(() => {
+  if (props.active) {
+    return 'hidden sm:block font-bold text-blue-600';
+  } else if (props.completed) {
+    return 'hidden sm:block text-green-500';
+  }
+  return 'hidden sm:block';
+});
+</script>
+<template>
+  <li class="flex items-center gap-2 bg-white p-2">
+    <span :class="spanClasses">
+      <span v-show="!completed">
+        <slot name="step" />
+      </span>
+      <i class="fa-solid fa-check" v-if="completed"></i>
+    </span>
+
+    <span :class="textClasses"> <slot name="description" /> </span>
+  </li>
+</template>
