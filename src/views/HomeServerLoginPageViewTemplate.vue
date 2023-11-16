37738b9dd6e2b5a5290aef097475b5b64efcd3f2
Philipp Stappert <philipp.stappert@student.kit.edu>
2023-11-16 15:23:34 +0100

    add templates for authentication (für Jakob)

---
+++ src/views/HomeServerLoginPageViewTemplate.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+
+import InputFieldWithLabelAndError from '@/components/input/InputFieldWithLabelAndError.vue';
+import LoginProcessBase from './partials/LoginProcessBase.vue';
+import LoginContinueButton from './partials/LoginContinueButton.vue';
+import LoginStepsBar from './partials/LoginStepsBar.vue';
+
+const loading = ref(false);
+</script>
+<template>
+  <LoginProcessBase>
+    <form action="#" class="mt-8 flex flex-col gap-6 w-full">
+      <LoginStepsBar :active="1" />
+      <div>
+        <InputFieldWithLabelAndError
+          type="text"
+          id="homeserver"
+          name="homeserver"
+          placeholder="z.B. https://matrix.org"
+          label="Homeserver-URL"
+        />
+      </div>
+
+      <div class="sm:flex sm:items-center sm:gap-4">
+        <LoginContinueButton :loading="loading" @continue="loading = true">
+          Weiter zum Anmelden <i class="fa-solid fa-arrow-right-to-bracket"></i>
+        </LoginContinueButton>
+      </div>
+    </form>
+  </LoginProcessBase>
+</template>
---
+++ src/views/partials/LoginStepsBar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import StepsBar from '@/components/steps/StepsBar.vue';
+import StepsBarStep from '@/components/steps/StepsBarStep.vue';
+
+defineProps({
+  active: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+<template>
+  <StepsBar>
+    <StepsBarStep :completed="active == 2" :active="active == 1">
+      <template #step> 1 </template>
+      <template #description> Homeserver </template>
+    </StepsBarStep>
+    <StepsBarStep :active="active == 2">
+      <template #step> 2 </template>
+      <template #description> Bei Matrix anmelden </template>
+    </StepsBarStep>
+  </StepsBar>
+</template>
