e282622bcec3c69e65c8308ed048f25039adb695
uelbi <uelbi@student.kit.edu>
2023-11-17 20:41:02 +0100

    implemented user model in type script

--- src/logic/models/User.ts
+++ src/logic/models/User.ts
@@ -1,3 +1,33 @@
+import Room from '@/logic/models/Room';
+
 class User{
-  //TODO
+  private rooms: Room[];
+  private name: string;
+  private matrixId: string;
+  private profilePictureURL: string;
+  private paymentInformation: string;
+  
+  constructor(name: string, matrixID: string, profilePictureURL: string, paymentInformation: string, rooms: Room[]) {
+    this.name = name;
+    this.matrixId = matrixID;
+    this.profilePictureURL = profilePictureURL;
+    this.paymentInformation = paymentInformation;
+    this.rooms = rooms;
+  }
+
+  getRooms() {
+    return this.rooms;
+  }
+  getName() {
+    return this.name;
+  }
+  getMatrixId() {
+    return this.matrixId;
+  }
+  getProfilePictureUrl() {
+    return this.profilePictureURL;
+  }
+  getPaymentInformation() {
+    return this.paymentInformation;
+  }
 }
