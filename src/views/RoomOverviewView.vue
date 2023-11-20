66888a13df085dc4e6d06c96e501cee0f8c7be08
Philipp Stappert <philipp.stappert@student.kit.edu>
2023-11-20 17:11:14 +0100

    make responsive

--- src/views/RoomOverviewView.vue
+++ src/views/RoomOverviewView.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import MainLayout from '@/layouts/MainLayout.vue';
-import ProfileImage from '@/components/media/ProfileImage.vue';
-
+import RoomTile from '@/views/partials/RoomTile.vue';
 </script>
 <template>
   <MainLayout>
@@ -17,94 +16,7 @@ import ProfileImage from '@/components/media/ProfileImage.vue';
 
     <!--Rooms-->
     <div class="flex flex-col items-center gap-2 p-2 lg:p-5">
-
-      <!--Start of a room-->
-      <div class="flex flex-row w-full lg:max-w-[80%] gap-2 p-5 rounded-lg border-r-8 border-b-8 bg-gray-100 border-gray-200">
-
-        <div>
-          <ProfileImage class="rounded-full w-32 h-32"></ProfileImage>
-        </div>
-          
-        <div class="flex flex-col flex-grow gap-2">
-
-        <!--Header-->
-          <div class="flex flex-row justify-center text-lg font-bold">
-            <h1>PSE</h1>
-          </div>
-
-          <!--Content-->
-          <div class="flex flex-row justify-center">
-            <!--column 1-->
-            <div class="basis-1/2 flex flex-col gap-5 text-center">
-              <div class="flex flex-col">
-                <span class="font-semibold"><i class="fa-solid fa-users"></i> 5 Mitglieder</span>
-                <span>2 online</span>
-              </div>
-              <div class="flex flex-col">
-                <span class="font-semibold"><i class="fa-solid fa-coins"></i> +5€</span>
-                <span>Geld fehlt von: Maxi</span>
-              </div>
-            </div>
-            <!--Column 2-->
-            <div class="basis-1/2 flex flex-col gap-5 text-center">
-              <div class="flex flex-col">
-                <span class="font-semibold"><i class="fa-solid fa-comments"></i> 2 ungelesene Nachrichten</span>
-                <span>Maxi: PSE läuft super</span>
-              </div>
-              <div class="flex flex-col">
-                <span class="font-semibold"><i class="fa-solid fa-building-columns"></i> 2 unbezahlte Rechnungen</span>
-                <span>Maxi, Maxim</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!--End of a room-->
-      
-      <!--Start of a room-->
-      <div class="flex flex-row w-full lg:max-w-[80%] gap-2 p-5 rounded-lg border-r-8 border-b-8 bg-gray-100 border-gray-200">
-
-        <div>
-          <ProfileImage class="rounded-full w-32 h-32"></ProfileImage>
-        </div>
-
-        <div class="flex flex-col flex-grow gap-2">
-
-          <!--Header-->
-          <div class="flex flex-row justify-center text-lg font-bold">
-            <h1>Fußball</h1>
-          </div>
-
-          <!--Content-->
-          <div class="flex flex-row justify-center">
-            <!--column 1-->
-            <div class="basis-1/2 flex flex-col gap-5 text-center">
-              <div class="flex flex-col">
-                <span class="font-semibold"><i class="fa-solid fa-users"></i> 30 Mitglieder</span>
-                <span>8 online</span>
-              </div>
-              <div class="flex flex-col">
-                <span class="font-semibold"><i class="fa-solid fa-coins"></i> -40€</span>
-                <span>Schulden an: Maxi, Maxim, Maximilian...</span>
-              </div>
-            </div>
-            <!--Column 2-->
-            <div class="basis-1/2 flex flex-col gap-5 text-center">
-              <div class="flex flex-col">
-                <span class="font-semibold"><i class="fa-solid fa-comments"></i> 5 ungelesene Nachrichten</span>
-                <span>Maxi: mir fehlt noch Geld...</span>
-              </div>
-              <div class="flex flex-col">
-                <span class="font-semibold"><i class="fa-solid fa-building-columns"></i> 8 unbezahlte Rechnungen</span>
-                <span>Max, Maxi, Maximilian...</span>
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
-      <!--End of a room-->
-
+      <RoomTile room="sldjflköajsdöfjalksdj" />
     </div>
     <!--End of rooms-->
   </MainLayout>
