439ab5aa52b205c7339c9a65893b133b0c9941dc
ubpch <ubpch@student.kit.edu>
2023-11-30 15:53:11 +0100

    Changed plus size

--- src/views/TransactionOverviewView.vue
+++ src/views/TransactionOverviewView.vue
@@ -42,7 +42,7 @@ import ProfileImage from '@/components/media/ProfileImage.vue';
                     <!--Plus button-->
                     <div class="flex flex-row h-24 justify-end items-center m-5">
                         <div class="flex rounded-full h-24 w-24 items-center justify-center bg-green-300 border-2 border-green-400">
-                            <i class="fa-solid fa-plus"></i>
+                            <i class="text-4xl fa-solid fa-plus"></i>
                         </div>
                     </div>
                 </div>
