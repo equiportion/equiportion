56cc1af7631f6c33b00fa9d92533e647e0a12fbf
ufgfg <ufgfg@student.kit.edu>
2023-11-30 14:22:56 +0100

    fixed custom event type prefix

--- src/logic/constants/eventTypes.ts
+++ src/logic/constants/eventTypes.ts
@@ -1,9 +1,11 @@
+const customEventTypePrefix = 'edu.kit.kastel.dsn.pse.';
+
 const eventTypes = {
   roomMember: 'm.room.member',
   roomName: 'm.room.name',
   roomAvatar: 'm.room.avatar',
   roomTopic: 'm.room.topic',
-  paymentInformation: 'edu.kit.dsn.pse.payment_information',
+  paymentInformation: customEventTypePrefix + 'payment_information',
 };
 
 export default eventTypes;
