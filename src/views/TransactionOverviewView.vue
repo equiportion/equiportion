5f944dc8ec5b9cacb0f5f7d3228fff679ba54c8b
ubpch <ubpch@student.kit.edu>
2023-12-04 23:55:34 +0100

    added comments

--- src/views/TransactionOverviewView.vue
+++ src/views/TransactionOverviewView.vue
@@ -11,6 +11,7 @@ import router from '@/router';
 import TransactionTile from './partials/TransactionTile.vue';
 import TransactionEvent from '@/logic/models/events/TransactionEvent';
 
+//sets up 2 test users for the text transaction
 const User2 = {
   user: "User2",
   amount: 10 as number,
@@ -20,15 +21,18 @@ const User3 = {
   amount: 10 as number,
 };
 
+//creates a test transaction
 const testTransaction = new TransactionEvent("abc","Pizza",20,"User1",[User2, User3], "12345")
 
 var client: AuthenticatedMatrixClient;
 const roomId = useRoute().params.roomId.toString();
 var room: Ref<Room | undefined> = ref();
-
 const loading = ref(true);
+
+//loads the necessary data
 useAuthenticatedMatrixClient(loadData);
 
+//function used in loading the necessary data
 async function loadData(clientInstance: AuthenticatedMatrixClient) {
   client = clientInstance;
   room.value = client.getRoom(roomId);
@@ -46,19 +50,23 @@ function newTransaction() {
 <template>
   <MainLayout>
     <div class="flex flex-col px-5 items-center">
+      <!--The main body of the transaction overview, being 4/6 wide-->
       <div class="flex flex-col lg:w-4/6 w-full">
         <!--Profile image and username -->
         <div class="flex h-40 flex-col items-center lg:flex-row mt-4">
+          <!--shows the room picture-->
           <MxcOrPlaceholderImage
             :mxcUrl="room?.getAvatarUrl() ?? ''"
             class="rounded-full w-16 h-16 lg:w-32 lg:h-32"
             :placeholderText="room?.getName() ?? '?'"
           />
           <div class="flex flex-col items-center lg:items-start lg:ml-4 lg:gap-5">
+            <!--shows the room name if possible or the room id if not-->
             <h1 class="flex text-3xl font-bold text-gray-900">
               {{ room?.getName() ?? roomId }}
             </h1>
             <div class="flex flex-col lg:flex-row lg:gap-2">
+              <!--shows the display names of all members in a room if possible or the member id if not-->
               <span v-for="member in room?.getMemberIds()" :key="member" class="flex truncate">
                 {{ client.getUser(member)?.getDisplayname() ?? client.getUser(member)?.getUserId()}}
             </span>
@@ -66,13 +74,15 @@ function newTransaction() {
           </div>
         </div>
         <div v-if="!loading && room" class="flex flex-col lg:mt-2">
-          <!--default message-->
+
+          <!--default message if no transactions were made-->
           <template v-if="room?.getTransactionEvents().length <= 0">
             <div class="flex flex-col text-sm text-gray-400 items-center mt-5">
               Keine Transaktionen vorhanden
             </div>
           </template>
 
+          <!--the header of the table containing the transactions-->
           <div v-else class="grid justify-center-center grid-cols-1 lg:grid-cols-3">
             <div class="col-span-3 lg:m-5 invisible lg:visible">
               <div class="flex flex-row">
@@ -81,6 +91,8 @@ function newTransaction() {
                 <div class="w-1/3 text-center font-bold text-gray-900">Schuldner</div>
               </div>
             </div>
+
+            <!--shows all transaction using the transacion tile partial-->
             <div
               class="mt-2 col-span-3 bg-gray-100 border-b-8 border-r-8 rounded border-gray-200"
               v-for="transaction in room.getTransactionEvents()"
@@ -90,7 +102,8 @@ function newTransaction() {
             </div>
           </div>
         </div>
-        <!--Plus button-->
+
+        <!--shows a button that enables the user to add a new transaction-->
         <div class="flex flex-row justify-end">
           <div class="static">
             <div class="absolute bottom-5 right-5 lg:left-1/2 lg:transform">
--- src/views/partials/TransactionTile.vue
+++ src/views/partials/TransactionTile.vue
@@ -12,16 +12,19 @@ const props = defineProps({
 
 <template>
     <div class="flex flex-col lg:flex-row m-2">
+        <!--first column shows the purpose of the transaction-->
         <div class="flex lg:w-1/3 justify-center mx-2 mt-2 lg:mt-0">
             <span class="flex flex-col justify-center truncate text-gray-700">
                 {{ transaction.getPurpose() }}
             </span>
         </div>
+        <!--second column shows the creditor and the amount paid of the transaction-->
         <div class="flex lg:w-1/3 justify-center mx-2 mt-2 lg:mt-0">
             <span class="flex flex-col justify-center truncate text-gray-700">
                 {{ transaction.getSum() }}€, gezahlt von {{ transaction.getCreditor() }}
             </span>
         </div>
+        <!--third column shows all debitors and the amount they owe-->
         <div class="flex flex-col lg:w-1/3 justify-center mx-2 mt-2 lg:mt-0">
             <span class="flex flex-col justify-center text-center truncate text-gray-700"
             v-for="debitor in transaction.getDebtors()"
