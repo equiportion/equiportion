f79f2e7c252b01229fa85662a0f08af6e1b5ecd4
ufgfg <ufgfg@student.kit.edu>
2023-12-04 13:51:47 +0100

    Added UserAvatar component

---
+++ src/components/media/UserAvatar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import User from '@/logic/models/User';
+import MxcOrPlaceholderImage from '@/components/media/MxcOrPlaceholderImage.vue';
+
+const props = defineProps({
+  user: {
+    type: User,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <MxcOrPlaceholderImage :mxcUrl="props.user.getAvatarUrl() ?? ''" :placeholderText="props.user.getDisplayname() ?? props.user.getUserId()" />
+</template>
