6ee0e4b78b0f0b5e12c13ef4a8a7e3af828c5285
Philipp Stappert <philipp.stappert@student.kit.edu>
2023-12-07 12:29:34 +0100

    use codequality instead

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,19 +4,19 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   'extends': [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   overrides: [
     {
       files: [
         '**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
+        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
       ],
       'extends': [
-        'plugin:cypress/recommended'
+        'plugin:cypress/recommended',
       ]
     }
   ],
