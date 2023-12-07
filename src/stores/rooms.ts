ab631721ea4959a5dbd4a5a63bea2e2aa06ccd9d
Philipp Stappert <philipp.stappert@student.kit.edu>
2023-12-07 16:28:11 +0100

    add function to get room & fix copypasta

--- src/stores/rooms.ts
+++ src/stores/rooms.ts
@@ -2,8 +2,17 @@ import type Room from '@/logic/models/Room';
 import {defineStore} from 'pinia';
 import {ref, type Ref} from 'vue';
 
-export const useLoggedInUserStore = defineStore('rooms', () => {
+export const useRoomsStore = defineStore('rooms', () => {
   const rooms: Ref<{[roomId: string]: Room}> = ref({});
+
+  /**
+   * Get a room by its roomId.
+   * @param {string} roomId the matrix-room-id of the room
+   * @returns {Room|undefined} the room if it exists, else undefined
+   */
+  function getRoom(roomId: string): Room | undefined {
+    return rooms.value[roomId];
+  }
 
   return {rooms};
 });
