d51c5342f8d1ae7bf3dab9c5ea9574ac923520e1
ufgfg <ufgfg@student.kit.edu>
2023-12-13 11:09:40 +0100

    jörns komischen test entfernt

--- cypress/e2e/roomOverview.cy.ts
+++ cypress/e2e/roomOverview.cy.ts
@@ -1,14 +1,9 @@
-import { login } from "../support/e2e";
+import {login} from '../support/e2e';
 
 describe('account-with-two-rooms-has-three-rooms-displayed', () => {
   it('account-with-two-rooms-has-two-rooms-displayed', () => {
-    login("two-filled-rooms")
-    cy.get('#main-layout').should('be.visible')
-    cy.get('#main-layout').get('#rooms').children().should('have.length', 2)
-  })
-  it('account-with-three-rooms-has-three-rooms-displayed', () => {
-    login("three-rooms")
-    cy.get('#main-layout').should('be.visible')
-    cy.get('#main-layout').get('#rooms').children().should('have.length', 3)
-  })
-})
+    login('two-filled-rooms');
+    cy.get('#main-layout').should('be.visible');
+    cy.get('#main-layout').get('#rooms').children().should('have.length', 2);
+  });
+});
