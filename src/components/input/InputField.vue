f6900ccadaee749412ea1f9cfe2609315304cac8
Philipp Stappert <philipp.stappert@student.kit.edu>
2023-12-14 11:42:12 +0100

    bind attributes

--- src/components/input/InputField.vue
+++ src/components/input/InputField.vue
@@ -47,5 +47,6 @@ const inputValue = computed({
     :name="name"
     class="w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
     :placeholder="placeholder"
+    v-bind="$attrs"
   />
 </template>
