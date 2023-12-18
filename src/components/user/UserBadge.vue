a3f43c6479e8113e4a47af54213f1342033f8464
Philipp Stappert <philipp.stappert@student.kit.edu>
2023-12-18 12:07:17 +0100

    multiple sizes of userbadge

--- src/components/user/UserBadge.vue
+++ src/components/user/UserBadge.vue
@@ -1,21 +1,49 @@
 <script setup lang="ts">
 import User from '@/logic/models/User';
 import MxcOrPlaceholderImage from '../media/MxcOrPlaceholderImage.vue';
+import {computed} from 'vue';
 
-defineProps({
+const props = defineProps({
   user: {
     type: User,
     required: true,
   },
+  size: {
+    type: String,
+    default: 'md',
+  },
+});
+
+const wrapperClasses = computed(() => {
+  const classes = 'flex flex-row bg-blue-600 text-gray-100 rounded-full items-center pr-3';
+  switch (props.size) {
+    case 'sm':
+      return classes + ' max-h-6 text-base';
+    case 'lg':
+      return classes + ' max-h-10 text-lg';
+    case 'md':
+    default:
+      return classes + ' max-h-8';
+  }
+});
+
+const imageClasses = computed(() => {
+  const classes = 'rounded-full';
+  switch (props.size) {
+    case 'sm':
+      return classes + ' w-6 h-6';
+    case 'lg':
+      return classes + ' w-10 h-10';
+    case 'md':
+    default:
+      return classes + ' w-8 h-8';
+  }
 });
 </script>
 <template>
-  <div
-    class="flex flex-row max-h-8 bg-blue-600 text-gray-100 rounded-full items-center pr-3"
-    :title="user.getUserId()"
-  >
+  <div :class="wrapperClasses" :title="user.getUserId()">
     <MxcOrPlaceholderImage
-      class="rounded-full w-8 h-8"
+      :class="imageClasses"
       :mxc-url="user.getAvatarUrl()"
       :placeholder-text="user.getDisplayname() ?? user.getUserId()"
     />
