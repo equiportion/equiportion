d3f41971430a920e6f6e2d65d8fe7882ce761fd3
ufgfg <ufgfg@student.kit.edu>
2024-01-27 12:02:53 +0100

    better phrasing in docs

--- src/logic/models/compensation/BipartiteCompensation.ts
+++ src/logic/models/compensation/BipartiteCompensation.ts
@@ -11,7 +11,7 @@ import Room from '@/logic/models/Room';
  */
 class BipartiteCompensation implements ICompensationAlgorithm {
   /**
-   * Calculates the compensation for the given room. This algorithm is optimal in the total value of compensation-transactions and
+   * Calculates the compensation for the given room. This algorithm is minimal in the total value of compensation-transactions and
    * produces no more than n-1 compensation-transactions, where n ist the number of users that have a balance-total != 0.
    * @param {Room} room the room
    * @return {{[userId: string]: number}} the compensation, a positive number means that the user has to pay, a negative number means that the user gets money
