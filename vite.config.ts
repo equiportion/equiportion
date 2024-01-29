eb00651e60821b3056ce122587782dd348b97fda
Philipp Stappert <philipp.stappert@student.kit.edu>
2024-01-29 22:37:36 +0100

    maybe so

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,29 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
       },
+      manifest: {
+        name: 'EquiPortion',
+        short_name: 'EquiPortion',
+        description:
+          'Aufteilen von Rechnungen war noch nie so einfach. Mit EquiPortion kannst du deine Rechnungen aufteilen und mit deinen Freunden teilen. Einfach, schnell und sicher über den Matrix-Server deines Vertrauens.',
+        theme_color: '#98e9e1',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: '@/assets/LogoSquare.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any maskable',
+          },
+          {
+            src: '@/assets/LogoSquare.png',
+            sizes: '1000x1000',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
     }),
   ],
   resolve: {
