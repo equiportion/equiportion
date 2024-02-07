e13b6078990378e5f9164352bbb49e7a631b3ce1
uelbi <uelbi@student.kit.edu>
2024-02-07 15:42:42 +0100

    vorbereitungen um beim login so richtig gas zu geben sobald dieses problem weggestubbed ist

--- cypress/e2e/login-and-logout.cy.ts
+++ cypress/e2e/login-and-logout.cy.ts
@@ -3,13 +3,6 @@
 import {standard, unauthenticated} from '../support/stubs';
 
 describe('login and logout', () => {
-  it('no error bombardement when homeserver is invalid', () => {
-    unauthenticated(() => {
-      cy.visit('http://localhost:5173/login');
-
-      cy.get('#username').type('account-empty-room:pse.');
-    });
-  });
   it('wrong password does not login', () => {
     unauthenticated(() => {
       cy.visit('http://localhost:5173/login');
@@ -17,22 +10,36 @@ describe('login and logout', () => {
       cy.get('#username').type('account-empty-room:pse.dsn.kastel.kit.edu');
       cy.get('#password').type('Dieses Passwort ist falsch!');
 
-      //TODO
+      //TODO stub server
+      //TODO click login button
+      //TODO check server request
+      //TODO send back login failiure
+      //TODO check front end
     });
   });
-  // it('correct login data logs in', () => {
-  //   login('no-rooms');
-  //   cy.get('#rooms').should('exist');
-  //   cy.get('#welcome-message-user', {timeout: 7000}).should('exist');
-  // });
-  // it('shows error message if homerserver invalid', () => {
-  //   cy.visit('http://localhost:5173/login');
-  //   cy.get('#username').type('@psetest:invalidhomeserver');
-  //   cy.get('#homeserver').type('testpassword321');
-  //   cy.get('#homeserverWarning').should('be.visible');
-  //   cy.get('#loginbutton').should(
-  //     'have.class',
-  //     'w-full disabled inline-block shrink-0 rounded-md border border-gray-200 bg-gray-400 px-12 py-3 text-sm font-medium text-white transition'
-  //   );
-  // });
+  it('correct password logs in', () => {
+    unauthenticated(() => {
+      cy.visit('http://localhost:5173/login');
+
+      cy.get('#username').type('account-empty-room:pse.dsn.kastel.kit.edu');
+      cy.get('#password').type('Dieses Passwort ist richtig!');
+
+      //TODO stub server
+      //TODO click login button
+      //TODO check server request
+      //TODO send back login success
+      //TODO check front end
+    });
+  });
+  it('user gets error-message if the homeserver is invalid', () => {
+    unauthenticated(() => {
+      cy.visit('http://localhost:5173/login');
+
+      cy.get('#username').type('account-empty-room:pse.');
+      cy.get('#homeserverWarning').should('be.visible');
+      cy.get('#loginbutton').should('be.disabled');
+
+      //TODO stub server
+    });
+  });
 });
