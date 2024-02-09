e8c7d4bc57410730ceadab2cefe2e2d9bce04808
Philipp Stappert <philipp.stappert@student.kit.edu>
2024-02-09 14:17:59 +0100

    improve message

--- src/views/partials/InviteModal.vue
+++ src/views/partials/InviteModal.vue
@@ -88,7 +88,7 @@ async function sendInvitation() {
     }, 3000);
   } else {
     userIdError.value =
-      'Einladung fehlgeschlagen: Bitte prüfe, ob der Benutzer existiert und ob du die Berechtigung hast, ihn einzuladen.';
+      'Einladung fehlgeschlagen: Bitte prüfe, ob der Benutzer existiert (und noch nicht in diesem Raum ist) und ob du die Berechtigung hast, ihn einzuladen.';
   }
 }
 </script>
@@ -100,7 +100,7 @@ async function sendInvitation() {
     <div class="flex flex-col items-center gap-2">
       <InputFieldWithLabelAndError
         v-model="userId"
-        label="Matrix-Benutzer-ID"
+        label="Matrix-ID"
         type="text"
         :error="userIdError"
         name="username"
