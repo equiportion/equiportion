9c9b395ede08342b1d5d2e702d159ed5b83b8fbe
uelbi <uelbi@student.kit.edu>
2024-02-20 12:46:42 +0100

    stubbed wrong password does not login

--- cypress/e2e/login-and-logout.cy.ts
+++ cypress/e2e/login-and-logout.cy.ts
@@ -3,37 +3,39 @@
 import {unauthenticated} from '../support/stubs';
 
 describe('login and logout', () => {
-  // it('wrong password does not login', () => {
-  //   unauthenticated(() => {
-  //     cy.visit('http://localhost:5173/login');
-  //     cy.get('#username').type('account-empty-room:pse.dsn.kastel.kit.edu');
-  //     cy.get('#password').type('Dieses Passwort ist falsch!');
-  //     //TODO stub server
-  //     //TODO click login button
-  //     //TODO check server request
-  //     //TODO send back login failiure
-  //     //TODO check front end
-  //   });
-  // });
-  // it('correct password logs in', () => {
-  //   unauthenticated(() => {
-  //     cy.visit('http://localhost:5173/login');
-  //     cy.get('#username').type('account-empty-room:pse.dsn.kastel.kit.edu');
-  //     cy.get('#password').type('Dieses Passwort ist richtig!');
-  //     //TODO stub server
-  //     //TODO click login button
-  //     //TODO check server request
-  //     //TODO send back login success
-  //     //TODO check front end
-  //   });
-  // });
-  // it('user gets error-message if the homeserver is invalid', () => {
-  //   unauthenticated(() => {
-  //     cy.visit('http://localhost:5173/login');
-  //     cy.get('#username').type('account-empty-room:pse.');
-  //     cy.get('#homeserverWarning').should('be.visible');
-  //     cy.get('#loginbutton').should('be.disabled');
-  //     //TODO stub server
-  //   });
-  // });
+  it('wrong password does not login', () => {
+    unauthenticated(() => {
+      cy.intercept({
+        url: '/_matrix/client/v3/login',
+        method: 'POST',
+      });
+      cy.visit('http://localhost:5173/login');
+      cy.get('#username').type('account-multiple-filled-rooms1:pse.dsn.kastel.kit.edu');
+      cy.get('#password').type('Dieses Passwort ist falsch!');
+      cy.get('#loginbutton').click();
+      cy.get('#login-form').children().eq(2).contains('Ungültiger Benutzername oder Passwort');
+      cy.get('#login-form').children().eq(3).contains('Ungültiger Benutzername oder Passwort');
+    });
+  });
+  it('correct password logs in', () => {
+    unauthenticated(() => {
+      cy.visit('http://localhost:5173/login');
+      cy.get('#username').type('account-multiple-filled-rooms1:pse.dsn.kastel.kit.edu');
+      cy.get('#password').type('Dieses Passwort ist richtig!');
+      cy.get('#loginbutton').click();
+      //TODO stub server
+      //TODO check server request
+      //TODO send back login success
+      //TODO check front end
+    });
+  });
+  it('user gets error-message if the homeserver is invalid', () => {
+    unauthenticated(() => {
+      cy.visit('http://localhost:5173/login');
+      cy.get('#username').type('account-empty-room:pse.');
+      cy.get('#homeserverWarning').should('be.visible');
+      cy.get('#loginbutton').should('be.disabled');
+      //TODO stub server
+    });
+  });
 });
