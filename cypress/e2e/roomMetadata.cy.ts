0deaefb38a6ab830e796004794d2bd682d5aeadc
usslz <usslz@student.kit.edu>
2024-02-23 22:59:17 +0100

    upload stub

--- cypress/e2e/roomMetadata.cy.ts
+++ cypress/e2e/roomMetadata.cy.ts
@@ -1,6 +1,37 @@
 import {authenticated} from '../support/stubs';
 
 describe('/', () => {
+  it('changes room avatar', () => {
+    authenticated(() => {
+      cy.intercept(
+        {
+          url: '/_matrix/media/v3/upload',
+          method: 'POST',
+        },
+        {
+          fixture: 'matrix_media_v3_upload',
+        }
+      ).as('uploadPost');
+      cy.intercept(
+        {
+          url: '/_matrix/client/v3/rooms/!AovfRJyqtfNuSlegxT:stub.pse.dsn.kastel.kit.edu/state/m.room.avatar/',
+          method: 'PUT',
+        },
+        {
+          fixture: '',
+        }
+      ).as('MRoomAvatarEventPut');
+      cy.visit('http://localhost:5173/');
+      cy.get('#rooms>div').eq(0).click();
+      cy.get('#changeRoomDataButtonSm').click();
+      cy.get('#uploadLabel').selectFile('cypress/fixtures/cat.jpg');
+      cy.get('#changeRoomDataSubmitSm').click();
+      cy.wait('@uploadPost');
+      cy.wait('@MRoomAvatarEventPut').then(({request}) => {
+        expect(request.body.url).to.eq('mxc://example.com/AQwafuaFswefuhsfAFAgsw');
+      });
+    });
+  });
   it('changes room name', () => {
     authenticated(() => {
       cy.intercept(
@@ -43,35 +74,6 @@ describe('/', () => {
       cy.wait('@MRoomTopicEventPut').then(({request}) => {
         expect(request.body.topic).to.eq('newTopic');
       });
-    });
-  });
-  it('changes room avatar', () => {
-    authenticated(() => {
-      cy.intercept(
-        {
-          url: '/_matrix/media/v3/upload',
-          method: 'POST',
-        },
-        {
-          fixture: '',
-        }
-      ).as('uploadPost');
-      cy.intercept(
-        {
-          url: '/_matrix/client/v3/rooms/!AovfRJyqtfNuSlegxT:stub.pse.dsn.kastel.kit.edu/state/m.room.avatar/',
-          method: 'PUT',
-        },
-        {
-          fixture: '',
-        }
-      ).as('MRoomAvatarEventPut');
-      cy.visit('http://localhost:5173/');
-      cy.get('#rooms>div').eq(0).click();
-      cy.get('#changeRoomDataButtonSm').click();
-      cy.get('#uploadLabel').selectFile('cypress/fixtures/cat.jpg');
-      cy.get('#changeRoomDataSubmitSm').click();
-      cy.wait('@uploadPost');
-      cy.wait('@MRoomAvatarEventPut');
     });
   });
 });
