fc95a6cc2b115250a6c1ab7fc8edc27f53be90ee
Philipp Stappert <philipp.stappert@student.kit.edu>
2024-03-05 14:00:48 +0100

    roundbutton dark

--- src/components/buttons/RoundButton.vue
+++ src/components/buttons/RoundButton.vue
@@ -38,22 +38,22 @@ const emit = defineEmits(['click']);
 
 const buttonClasses = computed(() => {
   if (props.loading) {
-    return 'disabled h-12 w-12 inline-block rounded-full border border-gray-200 bg-gray-400 text-white transition';
+    return 'disabled h-12 w-12 inline-block rounded-full border border-gray-200 dark:border-0 bg-gray-400 text-white transition';
   }
   if (props.disabled) {
-    return 'disabled h-12 w-12 inline-block rounded-full border border-gray-200 bg-gray-400 text-white transition';
+    return 'disabled h-12 w-12 inline-block rounded-full border border-gray-200 dark:border-0 bg-gray-400 text-white transition';
   }
   if (props.success) {
-    return 'h-12 w-12 inline-block rounded-full border border-green-600 bg-green-600 text-white hover:bg-white hover:text-green-600 focus:outline-none focus:ring active:text-green-500 transition';
+    return 'h-12 w-12 inline-block rounded-full border border-green-600 bg-green-600 text-white hover:bg-white dark:hover:bg-gray-800 hover:text-green-600 focus:outline-none focus:ring focus:ring-green-200 active:text-green-500 transition';
   }
   if (props.outlined) {
-    return 'h-12 w-12 inline-block rounded-full border border-blue-600 bg-white text-blue-600 hover:bg-blue-600 hover:text-white focus:outline-none focus:ring active:text-blue-500 transition';
+    return 'h-12 w-12 inline-block rounded-full border border-blue-600 bg-white dark:bg-gray-800 text-blue-600 hover:bg-blue-600 hover:text-white focus:outline-none focus:ring active:text-blue-500 transition';
   }
   if (props.danger) {
-    return 'h-12 w-12 inline-block rounded-full border border-red-600 bg-red-600 text-white hover:bg-white hover:text-red-600 focus:outline-none focus:ring active:text-red-500 transition';
+    return 'h-12 w-12 inline-block rounded-full border border-red-600 bg-red-600 text-white hover:bg-white dark:hover:bg-gray-800 hover:text-red-600 focus:outline-none focus:ring focus:ring-red-200 active:text-red-500 transition';
   }
 
-  return 'h-12 w-12 inline-block rounded-full border border-blue-600 bg-blue-600 text-white hover:bg-white hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 transition';
+  return 'h-12 w-12 inline-block rounded-full border border-blue-600 bg-blue-600 text-white hover:bg-white dark:hover:bg-gray-800 hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 transition';
 });
 </script>
 <template>
