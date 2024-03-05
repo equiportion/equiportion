cc7de5c4eacb8a4b56f2ab2b7cea7c88d84933f3
Philipp Stappert <philipp.stappert@student.kit.edu>
2024-03-05 13:52:49 +0100

    dark profile page

--- src/views/ProfilePageView.vue
+++ src/views/ProfilePageView.vue
@@ -13,10 +13,10 @@ const loggedInUser = useLoggedInUserStore().user;
       <div class="flex flex-col items-center lg:flex-row justify-center gap-5">
         <ProfileImage class="w-40 h-40 rounded-full" />
         <div class="flex flex-col items-center lg:items-start">
-          <h1 class="text-2xl font-bold text-gray-900 sm:text-3xl">
+          <h1 class="text-2xl font-bold text-gray-900 dark:text-gray-200 sm:text-3xl">
             {{ loggedInUser.getDisplayname() ?? loggedInUser.getUserId() }}
           </h1>
-          <span v-if="loggedInUser.getDisplayname()" class="text-md text-gray-500">
+          <span v-if="loggedInUser.getDisplayname()" class="text-md text-gray-500 dark:text-gray-400">
             {{ loggedInUser.getUserId() }}
           </span>
         </div>
