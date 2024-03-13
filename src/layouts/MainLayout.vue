a195f42f0ac55654578c9f5e23b892afad890d34
Philipp Stappert <philipp.stappert@student.kit.edu>
2024-03-13 11:54:50 +0100

    finish dark design

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,6 +8,7 @@ import DropdownButton from '@/components/dropdowns/DropdownButton.vue';
 import DropdownLink from '@/components/dropdowns/DropdownLink.vue';
 
 import LogoWide from '@/components/brand/LogoWide.vue';
+import LogoWideWhite from '@/components/brand/LogoWideWhite.vue';
 
 import useGlobalEventBus from '@/composables/useGlobalEventBus';
 import cookieNames from '@/logic/constants/cookieNames';
@@ -26,18 +27,19 @@ function emitClick(event: Event) {
 function logout() {
   setCookie(cookieNames.accessToken, '');
 
-  router.push({name: 'landing-page'});
+  router.push({name: 'login'});
 }
 </script>
 
 <template>
   <div @click="emitClick($event)">
-    <header class="fixed top-0 bg-gray-100 w-full z-50 shadow-md">
+    <header class="fixed top-0 bg-gray-100 dark:bg-gray-900 w-full z-50 shadow-md">
       <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
         <div class="flex items-center justify-end lg:justify-between">
           <!-- Logo on the left sisde -->
           <RouterLink :to="{name: 'home'}">
-            <LogoWide class="hidden lg:flex h-10" />
+            <LogoWide class="hidden lg:flex dark:hidden h-10" />
+            <LogoWideWhite class="hidden lg:dark:flex h-10" />
           </RouterLink>
 
           <!-- Buttons, Search and Profile Picture on the right side -->
@@ -46,7 +48,7 @@ function logout() {
             <RouterLink
               id="home-button"
               :to="{name: 'home'}"
-              class="block shrink-0 rounded-full bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700 w-10 h-10"
+              class="block shrink-0 rounded-full bg-white dark:bg-gray-600 p-2.5 text-gray-600 dark:text-gray-200 shadow-sm hover:text-gray-900 dark:hover:text-white lg:hover:scale-105 w-10 h-10"
             >
               <span class="sr-only">Raumübersicht</span>
               <div class="flex w-full h-full items-center">
@@ -70,7 +72,7 @@ function logout() {
       </div>
     </header>
 
-    <main class="w-full pt-[104px] min-h-screen">
+    <main class="w-full pt-[104px] min-h-screen dark:bg-gray-800">
       <slot />
     </main>
   </div>
